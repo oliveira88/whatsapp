ba5fb93da3e4ad25eb3e8d59901a4f05637d1237
Guilherme Oliveira <guilherme.oliveira@itixti.com.br>
2021-12-06 17:04:09 -0300

    Created theme

---
+++ lib/theme/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+//  ignore_for_file: constant_identifier_names
+class ColorTheme {
+  static const Primary = Color(0xFF1F2C33);
+  static const BackgroundPrimary = Color(0xFF101B21);
+  static const GreenPrimary = Color(0xFF17977D);
+  static const GreyPrimary = Color(0xFF8696A1);
+  static const BlackPrimary = Color(0xFF1A2730);
+}
