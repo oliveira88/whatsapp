9eed6b0fc9fdca3bc91ff21cbb223866ad6bdc49
Guilherme Oliveira <guilherme.oliveira@itixti.com.br>
2021-12-06 18:41:46 -0300

    Created chat page

---
+++ lib/views/chat/chat_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp/views/chat/chat_widget.dart';
+
+class ChatPage extends StatelessWidget {
+  const ChatPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Scaffold(
+        body: ListView.builder(
+          itemCount: 55,
+          itemBuilder: (context, itemBuilder) {
+            return Chat();
+          },
+        ),
+      ),
+    );
+  }
+}
