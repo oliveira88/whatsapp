019b7776c9ad79d022be38faa61ae035b28129d5
Guilherme Oliveira <guilherme.oliveira@itixti.com.br>
2021-12-07 10:38:54 -0300

    Added styling in chat widget

--- lib/views/chat/chat_widget.dart
+++ lib/views/chat/chat_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp/theme/theme.dart';
 
 class Chat extends StatelessWidget {
   const Chat({Key? key}) : super(key: key);
@@ -8,24 +9,37 @@ class Chat extends StatelessWidget {
     return Container(
       width: double.infinity,
       height: 70,
-      color: Colors.white,
-      margin: EdgeInsets.only(bottom: 12),
+      margin: EdgeInsets.only(top: 12),
       child: Container(
-        alignment: Alignment.center,
         padding: EdgeInsets.symmetric(horizontal: 16),
         child: Row(
           children: [
             CircleAvatar(
               backgroundColor: Colors.grey[500],
+              radius: 25,
             ),
             SizedBox(width: 16),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: const [
-                Text('Guilherme'),
-                Text('Quando terminar avisa ae'),
-              ],
+            Expanded(
+              flex: 2,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text(
+                    'Info',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Text(
+                    'Quando terminar avisa ae',
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(color: ColorTheme.GreyPrimary),
+                  ),
+                ],
+              ),
             ),
             Expanded(
               child: Align(
@@ -33,8 +47,11 @@ class Chat extends StatelessWidget {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: const [
-                    Text('Data'),
-                    Text('visto'),
+                    Text(
+                      '13/08/25',
+                      style: TextStyle(color: ColorTheme.GreyPrimary),
+                    ),
+                    false ? Text('visto') : Text(''),
                   ],
                 ),
               ),
