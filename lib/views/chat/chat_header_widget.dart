aacee48e41585132485b2282e852154b9df8aaf1
Guilherme Oliveira <gui.r.oliveira.88@gmail.com>
2021-12-13 12:41:13 -0300

    Changed ChatHeader to a StatelessWidget

--- lib/views/chat/chat_header_widget.dart
+++ lib/views/chat/chat_header_widget.dart
@@ -1,61 +1,72 @@
 import 'package:flutter/material.dart';
 
-PreferredSizeWidget ChatHeader(
-    {required String title, required String subtitle}) {
-  return AppBar(
-    titleSpacing: -4.0,
-    title: Row(
-      children: [
-        CircleAvatar(
-          radius: 16.0,
-          backgroundImage: null,
-          backgroundColor: Colors.grey[500],
-        ),
-        Expanded(
-          child: Container(
-            padding: const EdgeInsets.only(left: 8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  style: TextStyle(fontSize: 16),
-                  overflow: TextOverflow.ellipsis,
-                ),
-                Text(
-                  subtitle,
-                  style: TextStyle(fontSize: 12),
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ],
-            ),
+class ChatHeader extends StatelessWidget with PreferredSizeWidget {
+  ChatHeader({Key? key, required this.title, required this.subtitle})
+      : super(key: key);
+
+  final String title;
+  final String subtitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      titleSpacing: -4.0,
+      title: Row(
+        children: [
+          CircleAvatar(
+            radius: 16.0,
+            backgroundImage: null,
+            backgroundColor: Colors.grey[500],
           ),
-        )
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: TextStyle(fontSize: 16),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Text(
+                    subtitle,
+                    style: TextStyle(fontSize: 12),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+      actions: [
+        IconButton(
+          icon: Icon(
+            Icons.videocam,
+            color: Colors.white,
+          ),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: Icon(
+            Icons.call,
+            color: Colors.white,
+          ),
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: Icon(
+            Icons.more_vert,
+            color: Colors.white,
+          ),
+          onPressed: () {},
+        ),
       ],
-    ),
-    actions: [
-      IconButton(
-        icon: Icon(
-          Icons.videocam,
-          color: Colors.white,
-        ),
-        onPressed: () {},
-      ),
-      IconButton(
-        icon: Icon(
-          Icons.call,
-          color: Colors.white,
-        ),
-        onPressed: () {},
-      ),
-      IconButton(
-        icon: Icon(
-          Icons.more_vert,
-          color: Colors.white,
-        ),
-        onPressed: () {},
-      ),
-    ],
-    elevation: 0,
-  );
+      elevation: 0,
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
 }
