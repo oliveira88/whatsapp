9a74e5279c89aff95cd19a76a6d2fecef006e0c9
Guilherme Oliveira <gui.r.oliveira.88@gmail.com>
2021-12-13 18:57:47 -0300

    Added new colors to the theme

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -5,6 +5,8 @@ class ColorTheme {
   static const Primary = Color(0xFF1F2C33);
   static const BackgroundPrimary = Color(0xFF101B21);
   static const GreenPrimary = Color(0xFF17977D);
+  static const GreenSecondary = Color(0xFF01493C);
   static const GreyPrimary = Color(0xFF8696A1);
+  static const GreySecondary = Color(0xFF192128);
   static const BlackPrimary = Color(0xFF1A2730);
 }
