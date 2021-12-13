0ab8f3931bb1a687ded2bdd8f6079ff386f899fd
Guilherme Oliveira <gui.r.oliveira.88@gmail.com>
2021-12-13 19:08:37 -0300

    Added messages to the single chat page

--- lib/views/chat/single_chat_page.dart
+++ lib/views/chat/single_chat_page.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp/theme/theme.dart';
 import 'package:whatsapp/views/chat/chat_header_widget.dart';
+import 'package:whatsapp/views/chat/chat_input_message_widget.dart';
+import 'package:whatsapp/views/chat/chat_text_widget.dart';
+
+class Teste {
+  final String message;
+  final bool fromYou;
+
+  Teste(this.message, this.fromYou);
+
+  Teste.fromJson(Map<String, dynamic> json)
+      : message = json['message'],
+        fromYou = json['fromYou'];
+
+  Map<String, dynamic> toJson() => {
+        'message': message,
+        'fromYou': fromYou,
+      };
+}
 
 class SingleChat extends StatelessWidget {
   const SingleChat({Key? key}) : super(key: key);
@@ -8,6 +26,24 @@ class SingleChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final _textController = TextEditingController();
+    final List<Map<String, dynamic>> messages = [
+      {
+        'message': 'KKKKKKKKKKKKKKKKKKKKKKKKKKKKKK',
+        'fromYou': 'true',
+      },
+      {
+        'message': 'This is a short message.',
+        'fromYou': 'false',
+      },
+      {
+        'message': 'This is a relatively longer line of text.',
+        'fromYou': 'false',
+      },
+      {
+        'message': 'Hi!',
+        'fromYou': 'true',
+      },
+    ];
     return Scaffold(
       appBar: ChatHeader(
         title: 'MAIOR NOME DE TODOS LOS T',
@@ -15,92 +51,69 @@ class SingleChat extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Text('Bomdia'),
           Expanded(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Expanded(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 6, horizontal: 4),
-                        decoration: BoxDecoration(
-                          color: ColorTheme.Primary,
-                          borderRadius: BorderRadius.all(Radius.circular(50)),
-                        ),
-                        child: Row(
+            child: ListView.builder(
+              itemCount: messages.length,
+              itemBuilder: (context, index) {
+                return Flex(
+                  direction: Axis.horizontal,
+                  mainAxisAlignment: messages[index]['fromYou'] == 'true'
+                      ? MainAxisAlignment.end
+                      : MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 6,
+                        horizontal: 8,
+                      ),
+                      margin: const EdgeInsets.symmetric(
+                        vertical: 4,
+                        horizontal: 8.0,
+                      ),
+                      constraints: BoxConstraints(
+                        maxWidth: MediaQuery.of(context).size.width * 0.7,
+                        minWidth: MediaQuery.of(context).size.width * 0.2,
+                      ),
+                      decoration: messages[index]['fromYou'] == 'true'
+                          ? BoxDecoration(
+                              color: ColorTheme.GreenSecondary,
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(12.0),
+                              ),
+                            )
+                          : BoxDecoration(
+                              color: ColorTheme.Primary,
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(12.0),
+                              ),
+                            ),
+                      child: Container(
+                        margin: EdgeInsets.only(bottom: 14),
+                        child: Stack(
+                          clipBehavior: Clip.none,
                           children: [
-                            IconButton(
-                              onPressed: () {},
-                              icon: Icon(
-                                Icons.emoji_emotions,
-                                color: ColorTheme.GreyPrimary,
+                            Positioned(
+                              top: 18,
+                              right: 0,
+                              child: Text(
+                                '13:03',
+                                style: TextStyle(color: ColorTheme.GreyPrimary),
                               ),
                             ),
-                            Flexible(
-                              child: TextField(
-                                controller: _textController,
-                                cursorColor: ColorTheme.GreenPrimary,
-                                textCapitalization:
-                                    TextCapitalization.sentences,
-                                cursorWidth: 2,
-                                decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: 'Message',
-                                  hintStyle: TextStyle(
-                                    color: ColorTheme.GreyPrimary,
-                                  ),
-                                ),
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 18),
-                              ),
+                            Text(
+                              messages[index]['message'] ?? '',
+                              style: TextStyle(color: Colors.white),
                             ),
-                            IconButton(
-                              onPressed: () {},
-                              icon: Icon(
-                                Icons.attach_file,
-                                color: ColorTheme.GreyPrimary,
-                              ),
-                            ),
-                            _textController.text.isEmpty
-                                ? IconButton(
-                                    onPressed: () {},
-                                    icon: Icon(
-                                      Icons.camera_alt,
-                                      color: ColorTheme.GreyPrimary,
-                                    ),
-                                  )
-                                : SizedBox(),
                           ],
                         ),
                       ),
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(vertical: 6, horizontal: 4),
-                  decoration: BoxDecoration(
-                    color: ColorTheme.GreenPrimary,
-                    shape: BoxShape.circle,
-                  ),
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      _textController.text.isEmpty
-                          ? Icons.keyboard_voice
-                          : Icons.send,
-                      color: Colors.white,
                     ),
-                  ),
-                )
-              ],
+                  ],
+                );
+              },
             ),
           ),
+          ChatInputMessageWidget(textController: _textController),
         ],
       ),
     );
